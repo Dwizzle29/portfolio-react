8d476a39de3047e3d19377e0e76408b3532aa07f
Dwizzle29 <77puppy77@gmail.com>
2024-09-04 19:46:12 +1000

    updates to form

--- src/Pages/ContactMe.jsx
+++ src/Pages/ContactMe.jsx
@@ -1,8 +1,10 @@
 export default function Contact() {
   return (
     <>
-      <h1>Contact Me section</h1>
-      <section className="main-content"></section>
+      <section className="main-content">
+        <h1>Contact Me section</h1>
+        <p className="contact-info">Email Me @ mikayla.d299@gmail.com</p>
+      </section>
     </>
   );
 }
