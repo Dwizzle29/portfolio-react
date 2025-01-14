f14c97489cb8a0336454d1affdda1c9b629354b7
Dwizzle29 <77puppy77@gmail.com>
2025-01-14 18:23:33 +0000

    project updats

--- src/Pages/MyPortfolio.jsx
+++ src/Pages/MyPortfolio.jsx
@@ -1,6 +1,9 @@
 import screenjot from "../assets/screenjot.png";
 import mikaylascorner from "../assets/mikaylascorner.png";
+import teamjacobvolleyball from "../assets/teamjacobvolleyball.png";
+import harmonivinyls from "../assets/harmonivinyls.png";
 import comingsoon from "../assets/comingsoon.png";
+
 export default function Portfolio() {
   return (
     <>
@@ -50,7 +53,52 @@ export default function Portfolio() {
           <div className="projects-portfolio-container">
             <div>
               <div className="projects-portfolio-card">
-                <img src={comingsoon} alt="Coming Soon" />
+                <img src={teamjacobvolleyball} alt="teamjacobvolleyball" />
+                <div className="projects-portfolio-info">
+                  <h3>Team Jacob Volleyball</h3>
+                  <p>
+                    <a href="https://teamjacobvolleyball.netlify.app/">
+                      Learn More
+                    </a>
+                  </p>
+                  <p>
+                    A design site created for my Volleyball Team Team Jacob in
+                    the class DDD20015
+                  </p>
+                  <p>HTML, CSS, React</p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="projects-portfolio-container">
+            <div>
+              <div className="projects-portfolio-card">
+                <img src={harmonivinyls} alt="harmonivinyls" />
+                <div className="projects-portfolio-info">
+                  <h3>harmoni vinyls</h3>
+                  <p>
+                    <a href="https://harmonivinyls.netlify.app/">Learn More</a>
+                  </p>
+                  <p>Website created for a design class that sells vinyls</p>
+                  <p>HTML, CSS, React</p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="projects-portfolio-container">
+            <div>
+              <div className="projects-portfolio-card">
+                <img src={comingsoon} alt="comingsoon" />
+                <div className="projects-portfolio-info">
+                  <h3>Coming Soon</h3>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="projects-portfolio-container">
+            <div>
+              <div className="projects-portfolio-card">
+                <img src={comingsoon} alt="comingsoon" />
                 <div className="projects-portfolio-info">
                   <h3>Coming Soon</h3>
                 </div>
